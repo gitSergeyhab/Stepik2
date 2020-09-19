8e998f7e6cd1e6845fed63a574a554bbbba75c6c
gitSergeyhab <redmoloch@yandex.ru>
2020-09-19 15:34:10 +0300

    fixed view - class MainView(ListView): add get_context_data() + fixed index.html

--- Djum/job/views.py
+++ Djum/job/views.py
@@ -5,11 +5,17 @@ from django.views import View
 from django.views.generic import ListView, DetailView
 from .models import Specialty, Company, Vacancy
 
-
+title = 'Джуманджи'
 class MainView(ListView):
     model = Specialty
     template_name = 'job/index.html'
+    context_object_name = 'MVspec'
+    extra_context = {'title': title}
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(MainView, self).get_context_data(**kwargs)
+        context['company'] = Company.objects.all()
+        return context
 
 # – Все вакансии списком   /vacancies
 class ListVacancy(ListView):
