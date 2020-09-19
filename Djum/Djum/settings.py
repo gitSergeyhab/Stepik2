fb2f2ca3affce5fdfbca009a1bcdec06db75727c
gitSergeyhab <redmoloch@yandex.ru>
2020-09-19 13:04:53 +0300

    add static to settings.py and base.html

--- Djum/Djum/settings.py
+++ Djum/Djum/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# нужно для работы на true сервере:
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# нужно для работы на отладочном сервере:
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Djum/static'),
+]
